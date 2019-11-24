fd860cac5dd386ae4ee4ad6532f9625fb6823414
Eyal Kalderon <ebkalderon@gmail.com>
2019-11-24 15:59:10 +0800

    Replicate minimal sandbox example from Bubblewrap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::process::Command;
 
-use bastille::Sandbox;
+use bastille::{Mapping, Sandbox};
 
 fn main() {
     Sandbox::new()
-        .enable_network(false)
+        .mount(Mapping::from_parts("/usr", "/usr", false).unwrap())
+        .mount(Mapping::from_parts("/dev", "/dev", true).unwrap())
+        .soft_link("usr/lib64", "/lib64")
+        .enable_network(true)
         .enable_sysctl(true)
-        .spawn(&mut Command::new("/bin/bash").env_clear())
+        .spawn(&mut Command::new("bash").env_clear())
         .expect("Failed to spawn process in sandbox")
         .wait()
         .expect("Failed to wait on spawned process");
