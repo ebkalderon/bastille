c0f0154aed46d390447e30730c1fbf86d7eb98a9
Eyal Kalderon <ebkalderon@gmail.com>
2019-12-03 23:17:56 +0800

    Update bash example to work on macOS

--- examples/bash.rs
+++ examples/bash.rs
@@ -2,11 +2,27 @@ use std::process::Command;
 
 use bastille::{Mapping, Sandbox};
 
+#[cfg(target_os = "linux")]
 fn main() {
     Sandbox::new()
         .mount(Mapping::from_parts("/usr", "/usr", false).unwrap())
         .mount(Mapping::from_parts("/dev", "/dev", true).unwrap())
         .soft_link("usr/lib64", "/lib64")
+        .allow_devices(true)
+        .allow_network(false)
+        .allow_sysctl(false)
+        .spawn(&mut Command::new("bash").env_clear())
+        .expect("Failed to spawn process in sandbox")
+        .wait()
+        .expect("Failed to wait on spawned process");
+}
+
+#[cfg(target_os = "macos")]
+fn main() {
+    Sandbox::new()
+        .mount(Mapping::from_parts("/bin", "/bin", false).unwrap())
+        .mount(Mapping::from_parts("/usr", "/usr", false).unwrap())
+        .mount(Mapping::from_parts("/dev", "/dev", true).unwrap())
         .allow_devices(true)
         .allow_network(false)
         .allow_sysctl(false)
